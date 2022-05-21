4df87a9b0b6f4b11497d52e13712719f84c387a8
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-21 10:42:12 -0400

    Add model test and complete user story 2

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe Astronaut, type: :model do
+  before do
+    @buzz = Astronaut.create!(name: "Buzz", age: 32, job: "Engineer")
+    @neil = Astronaut.create!(name: "Neil", age: 36, job: "Co-Pilot")
+    @sally = Astronaut.create!(name: "Sally", age: 34, job: "Pilot")
+  end
+
   describe 'Validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
@@ -10,5 +16,12 @@ describe Astronaut, type: :model do
   describe 'Relationships' do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
+  end
+
+  describe 'class methods' do
+    it "finds the average age of all astronauts" do
+      expect(Astronaut.average_age).to eq(34)
+    end
+    
   end
 end
